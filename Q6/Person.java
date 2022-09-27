e5e68955a1baa5e4f69a2fc97ca57c7a528d45b7
Gabriel <112896989+Gstein77@users.noreply.github.com>
2022-09-26 23:43:03 -0300

    Add files via upload

---
+++ Q6/Person.java
@@ -0,0 +1,6 @@
+package Q6;
+
+public abstract class Person implements IEntityValidator{
+    public abstract String toString();
+    
+}
---
+++ Q6/PersonLegal.java
@@ -0,0 +1,39 @@
+package Q6;
+
+public class PersonLegal extends Person{
+    private String cnpj;
+
+    public PersonLegal(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    @Override
+    public boolean isValid() {
+        String[] f = this.cnpj.split("/");
+        String[] i = f[0].split(".");//Algum erro que o i não pega os valores;
+        String[] g = f[1].split("-");
+        if(f.length != 2){
+            return false;
+        }else if(g.length!=2){
+            return false;
+        }else if(g[0]!="0001"){
+            return false;
+        }else if(g[1].length()!=2){
+            return false;
+        }else if(i.length!=3){
+            return false;
+        }else{
+            if(i[0].length()!=2 || i[1].length()!=3 || i[2].length()!=3){
+                return false;
+            }
+            return true;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "CNPJ: "+cnpj;
+    }
+
+    
+}
